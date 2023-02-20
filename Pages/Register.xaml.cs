2772ba5bdc8b293c693fb61808b927389f957eec
Jayasri <jayadhanasvs@gmail.com>
2023-02-20 10:19:18 +0530

    Add project files.

---
+++ Pages/Register.xaml.cs
@@ -0,0 +1,13 @@
+namespace MauiApp1.Pages;
+
+public partial class Register : ContentPage
+{
+	public Register()
+	{
+		InitializeComponent();
+	}
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+
+    }
+}
